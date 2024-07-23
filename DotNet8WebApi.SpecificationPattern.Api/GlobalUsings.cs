bc5bcd9b471c64ab8098e395b8aeec9d0bd5f585
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-23 17:08:39 +0630

    [Added] Global Usings

---
+++ DotNet8WebApi.SpecificationPattern.Api/GlobalUsings.cs
@@ -0,0 +1,9 @@
+// Global using directives
+
+global using DotNet8WebApi.SpecificationPattern.DbService.AppDbContexts;
+global using DotNet8WebApi.SpecificationPattern.Mapper;
+global using DotNet8WebApi.SpecificationPattern.Models.Features;
+global using DotNet8WebApi.SpecificationPattern.Models.Features.Blog;
+global using DotNet8WebApi.SpecificationPattern.Repositories.Features;
+global using DotNet8WebApi.SpecificationPattern.Repositories.Features.Specifications.Blog;
+global using DotNet8WebApi.SpecificationPattern.Shared;
