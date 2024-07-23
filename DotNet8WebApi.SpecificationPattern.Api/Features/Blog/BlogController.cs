2b40517008c27166a999885a0a752a0b089262fe
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-23 17:10:10 +0630

    Format BlogController

--- DotNet8WebApi.SpecificationPattern.Api/Features/Blog/BlogController.cs
+++ DotNet8WebApi.SpecificationPattern.Api/Features/Blog/BlogController.cs
@@ -1,58 +1,54 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿namespace DotNet8WebApi.SpecificationPattern.Api.Features.Blog;
 
-namespace DotNet8WebApi.SpecificationPattern.Api.Features.Blog
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly BL_Blog _bL_Blog;
+
+    public BlogController(BL_Blog bL_Blog)
     {
-        private readonly BL_Blog _bL_Blog;
+        _bL_Blog = bL_Blog;
+    }
 
-        public BlogController(BL_Blog bL_Blog)
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs()
+    {
+        var result = await _bL_Blog.GetBlogs();
+        return Content(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetBlogById(int id)
+    {
+        var result = await _bL_Blog.GetBlogById(id);
+        return Content(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateBlog([FromBody] BlogRequestModel requestModel)
+    {
+        var result = requestModel.IsValid();
+        if (result.IsError)
         {
-            _bL_Blog = bL_Blog;
+            return BadRequest(result);
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetBlogs()
-        {
-            var result = await _bL_Blog.GetBlogs();
-            return Content(result);
-        }
+        result = await _bL_Blog.CreateBlog(requestModel);
+        return Content(result);
+    }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetBlogById(int id)
-        {
-            var result = await _bL_Blog.GetBlogById(id);
-            return Content(result);
-        }
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> PatchBlog([FromBody] BlogRequestModel requestModel, int id)
+    {
+        var result = await _bL_Blog.PatchBlog(requestModel, id);
+        return Content(result);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateBlog([FromBody] BlogRequestModel requestModel)
-        {
-            var result = requestModel.IsValid();
-            if (result.IsError)
-            {
-                return BadRequest(result);
-            }
-
-            result = await _bL_Blog.CreateBlog(requestModel);
-            return Content(result);
-        }
-
-        [HttpPatch("{id}")]
-        public async Task<IActionResult> PatchBlog([FromBody] BlogRequestModel requestModel, int id)
-        {
-            var result = await _bL_Blog.PatchBlog(requestModel, id);
-            return Content(result);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBlog(int id)
-        {
-            var result = await _bL_Blog.DeleteBlog(id);
-            return Content(result);
-        }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBlog(int id)
+    {
+        var result = await _bL_Blog.DeleteBlog(id);
+        return Content(result);
     }
 }
--- DotNet8WebApi.SpecificationPattern.Api/GlobalUsings.cs
+++ DotNet8WebApi.SpecificationPattern.Api/GlobalUsings.cs
@@ -7,3 +7,4 @@ global using DotNet8WebApi.SpecificationPattern.Models.Features.Blog;
 global using DotNet8WebApi.SpecificationPattern.Repositories.Features;
 global using DotNet8WebApi.SpecificationPattern.Repositories.Features.Specifications.Blog;
 global using DotNet8WebApi.SpecificationPattern.Shared;
+global using Microsoft.AspNetCore.Mvc;
