b7e9374c3c48bdeb44afed25b1bbad53dc17f863
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-23 17:15:18 +0630

    Format Result Pattern

--- DotNet8WebApi.SpecificationPattern.Models/Features/Result.cs
+++ DotNet8WebApi.SpecificationPattern.Models/Features/Result.cs
@@ -1,69 +1,101 @@
-﻿using DotNet8WebApi.SpecificationPattern.Models.Enums;
-using DotNet8WebApi.SpecificationPattern.Models.Resources;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8WebApi.SpecificationPattern.Models.Features;
 
-namespace DotNet8WebApi.SpecificationPattern.Models.Features
+public class Result<T>
 {
-    public class Result<T>
+    public T Data { get; set; }
+    public string Message { get; set; }
+    public EnumStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; }
+    public bool IsError => !IsSuccess;
+
+    public static Result<T> SuccessResult(
+        string message = "Success.",
+        EnumStatusCode statusCode = EnumStatusCode.Success
+    )
     {
-        public T Data { get; set; }
-        public string Message { get; set; }
-        public EnumStatusCode StatusCode { get; set; }
-        public bool IsSuccess { get; set; }
-        public bool IsError => !IsSuccess;
-
-        public static Result<T> SuccessResult(string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
+        return new Result<T>
         {
-            return new Result<T> { Message = message, StatusCode = statusCode, IsSuccess = true };
-        }
+            Message = message,
+            StatusCode = statusCode,
+            IsSuccess = true
+        };
+    }
 
-        public static Result<T> SuccessResult(T data, string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
+    public static Result<T> SuccessResult(
+        T data,
+        string message = "Success.",
+        EnumStatusCode statusCode = EnumStatusCode.Success
+    )
+    {
+        return new Result<T>
         {
-            return new Result<T> { Data = data, Message = message, StatusCode = statusCode, IsSuccess = true };
-        }
+            Data = data,
+            Message = message,
+            StatusCode = statusCode,
+            IsSuccess = true
+        };
+    }
 
-        public static Result<T> FailureResult(string message = "Fail.", EnumStatusCode statusCode = EnumStatusCode.BadRequest)
+    public static Result<T> FailureResult(
+        string message = "Fail.",
+        EnumStatusCode statusCode = EnumStatusCode.BadRequest
+    )
+    {
+        return new Result<T>
         {
-            return new Result<T> { Message = message, StatusCode = statusCode, IsSuccess = false };
-        }
+            Message = message,
+            StatusCode = statusCode,
+            IsSuccess = false
+        };
+    }
 
-        public static Result<T> FailureResult(Exception ex)
+    public static Result<T> FailureResult(Exception ex)
+    {
+        return new Result<T>
         {
-            return new Result<T> { Message = ex.ToString(), StatusCode = EnumStatusCode.InternalServerError, IsSuccess = false };
-        }
+            Message = ex.ToString(),
+            StatusCode = EnumStatusCode.InternalServerError,
+            IsSuccess = false
+        };
+    }
 
-        public static Result<T> NotFoundResult(string message = "No Data Found.")
+    public static Result<T> NotFoundResult(string message = "No Data Found.")
+    {
+        return new Result<T>
         {
-            return new Result<T> { Message = message, StatusCode = EnumStatusCode.NotFound, IsSuccess = false };
-        }
+            Message = message,
+            StatusCode = EnumStatusCode.NotFound,
+            IsSuccess = false
+        };
+    }
 
-        public static Result<T> DuplicateResult(string message = "Duplicate Data.")
+    public static Result<T> DuplicateResult(string message = "Duplicate Data.")
+    {
+        return new Result<T>
         {
-            return new Result<T> { Message = message, StatusCode = EnumStatusCode.Conflict, IsSuccess = false };
-        }
+            Message = message,
+            StatusCode = EnumStatusCode.Conflict,
+            IsSuccess = false
+        };
+    }
 
-        public static Result<T> ExecuteResult(int result)
-        {
-            return result > 0 ? Result<T>.SuccessResult() : Result<T>.FailureResult();
-        }
+    public static Result<T> ExecuteResult(int result)
+    {
+        return result > 0 ? Result<T>.SuccessResult() : Result<T>.FailureResult();
+    }
 
-        public static Result<T> SaveSuccessResult()
-        {
-            return Result<T>.SuccessResult(MessageResource.SaveSuccess);
-        }
+    public static Result<T> SaveSuccessResult()
+    {
+        return Result<T>.SuccessResult(MessageResource.SaveSuccess);
+    }
 
-        public static Result<T> UpdateSuccessResult()
-        {
-            return Result<T>.SuccessResult(MessageResource.UpdateSuccess);
-        }
+    public static Result<T> UpdateSuccessResult()
+    {
+        return Result<T>.SuccessResult(MessageResource.UpdateSuccess);
+    }
 
-        public static Result<T> DeleteSuccessResult()
-        {
-            return Result<T>.SuccessResult(MessageResource.DeleteSuccess);
-        }
+    public static Result<T> DeleteSuccessResult()
+    {
+        return Result<T>.SuccessResult(MessageResource.DeleteSuccess);
     }
 }
--- DotNet8WebApi.SpecificationPattern.Models/GlobalUsings.cs
+++ DotNet8WebApi.SpecificationPattern.Models/GlobalUsings.cs
@@ -1,3 +1,5 @@
 // Global using directives
 
+global using DotNet8WebApi.SpecificationPattern.Models.Enums;
+global using DotNet8WebApi.SpecificationPattern.Models.Resources;
 global using DotNet8WebApi.SpecificationPattern.Shared;
