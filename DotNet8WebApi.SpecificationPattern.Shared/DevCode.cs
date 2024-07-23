7c719321a1f03fed49380b81c2af9781fe87d65f
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-23 17:19:23 +0630

    [Complete] Specification with Generic Repository

--- DotNet8WebApi.SpecificationPattern.Shared/DevCode.cs
+++ DotNet8WebApi.SpecificationPattern.Shared/DevCode.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8WebApi.SpecificationPattern.Shared;
 
-namespace DotNet8WebApi.SpecificationPattern.Shared
+public static class DevCode
 {
-    public static class DevCode
-    {
-        public static bool IsNullOrEmpty(this string str) =>
-            string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
-    }
+    public static bool IsNullOrEmpty(this string str) =>
+        string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
 }
