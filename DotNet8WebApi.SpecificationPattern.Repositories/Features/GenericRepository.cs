bb31aa1f04e612c22d9786156642ec159e38ebd9
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-23 15:02:52 +0630

    [Remove] Unnecessary Code

--- DotNet8WebApi.SpecificationPattern.Repositories/Features/GenericRepository.cs
+++ DotNet8WebApi.SpecificationPattern.Repositories/Features/GenericRepository.cs
@@ -27,12 +27,6 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
         await _dbSet.AddAsync(model);
     }
 
-    //public async Task<Result<List<T>>> GetAllAsync(ISpecification<T> specification)
-    //{
-    //    var lst = await _dbSet.AsQueryable().ApplySpecification(specification).ToListAsync();
-    //    return Result<List<T>>.SuccessResult(lst);
-    //}
-
     public async Task<Result<List<T>>> GetAllAsync(ISpecification<T> specification)
     {
         Result<List<T>> responseModel;
